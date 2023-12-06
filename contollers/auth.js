bfc1f12c1c75f37269984e5475c7b063f63c6bcb
MUDDUSSIR AWAN <muddussir1247@gmail.com>
2023-12-06 16:41:04 +0500

    Update auth.js

--- contollers/auth.js
+++ contollers/auth.js
@@ -1,9 +1,12 @@
+const jwt = require("jsonwebtoken");
 const { createUser, findUser } = require("../model/user");
 const bcrypt = require("bcrypt");
+const { SECRET_KEY } = require("../data/key");
 
 exports.createUser = async (email, password) => {
   try {
-    const resp = await createUser(email, password);
+    const hashPass = await bcrypt.hash(password, 12);
+    const resp = await createUser(email, hashPass);
     return resp;
   } catch (err) {
     throw err;
@@ -16,7 +19,8 @@ exports.login = async (email, password) => {
     const result = bcrypt.compare(password, !!user && user.password);
     console.log(result);
     if (result) {
-      return "Login Successfully";
+      var token = jwt.sign({ email }, SECRET_KEY, { expiresIn: "1h" });
+      return {token};
     }
     return "Incorrect Email Id or Pass";
   } catch (err) {
